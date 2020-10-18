36b16e3d727627ef89ff3068d917a3072b20146e
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-10-18 11:58:44 +0200

    Add Hardware setup description

--- src/multisensor.py
+++ src/multisensor.py
@@ -1,8 +1,15 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 '''
-    Description:    Use Open-ZWave with Aeotec Multisensor 6
+    Description:    Use Open-ZWave with Aeotec Z-Stick Gen. 5 and Multisensor 6
     Dependencies:   Open-ZWave, TBD
+
+    Hardware setup: 1.  Z-Stick Gen. 5 is plugged into an USB port
+                        and Z-Stick is registered as "/dev/ttyACM0" 
+                        (as per default).
+                    2.  Multisensor should be wired to a power source,
+                        i.e. plug it into an USB port to power it,
+                        or it will probably be sleeping most of the time.
 
 '''
 import sys
