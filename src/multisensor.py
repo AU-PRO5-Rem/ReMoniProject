9b456a616e6fd73ad7a983a2e7711b101d31530e
VintherWolf <danielvinther@youmail.dk>
2020-10-20 20:28:50 +0200

    Fix PEP defects and add doc string

--- src/multisensor.py
+++ src/multisensor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-'''
+"""
     Description:    Use Open-ZWave with Aeotec Z-Stick Gen. 5 and Multisensor 6
     Dependencies:   Open-ZWave, TBD
 
@@ -11,7 +11,7 @@
                         i.e. plug it into an USB port to power it,
                         or it will probably be sleeping most of the time.
 
-'''
+"""
 import sys
 
 from openzwave.node import ZWaveNode
@@ -26,11 +26,18 @@ import time
 Z_STICK = "/dev/ttyACM0"
 
 
-def is_zwave_network_awake(network):
+def is_zwave_network_awake(network_obj):
+    """Check if state of network is Awake
+
+    :param network_obj: initialized network object
+    :type network_obj: ZWaveNetwork Object
+    :return: True or False
+    :rtype: bool
+    """
     print("Waiting for Z-Stick Network to be awake")
     time_elapsed = 0
     for i in range(0, 20):
-        if network.state >= network.STATE_AWAKED:
+        if network_obj.state >= network_obj.STATE_AWAKED:
             print("Success: Z-Stick Network is Awake")
             break
         else:
@@ -38,7 +45,7 @@ def is_zwave_network_awake(network):
             time.sleep(1.0)
 
             # Write to display to indicate that network is still sleeping
-            if (i % 2 == 0):
+            if i % 2 == 0:
                 sys.stdout.write("zz")
             else:
                 sys.stdout.write("Z")
@@ -46,7 +53,7 @@ def is_zwave_network_awake(network):
             print("\n")
             return True
 
-    if network.state < network.STATE_AWAKED:
+    if network_obj.state < network_obj.STATE_AWAKED:
         print("Error: Network could not wake up!")
         print("\n")
         return False
@@ -64,58 +71,69 @@ def zwave_network_scan():
         network.controller.node.version))
     print("Nodes in network : {}".format(network.nodes_count))
     for node in network.nodes:
-        print("------------------------------------------------------------")
+        print("---------------------------------------")
         print(
-            "{} - Name : {}".format(network.nodes[node].node_id, network.nodes[node].name))
+            "{} - Name : {}".format(network.nodes[node].node_id,
+                                    network.nodes[node].name))
         print("{} - Manufacturer name / id : {} / {}".format(
-            network.nodes[node].node_id, network.nodes[node].manufacturer_name, network.nodes[node].manufacturer_id))
+            network.nodes[node].node_id,
+            network.nodes[node].manufacturer_name,
+            network.nodes[node].manufacturer_id))
         print("{} - Product name / id / type : {} / {} / {}".format(
-            network.nodes[node].node_id, network.nodes[node].product_name, network.nodes[node].product_id, network.nodes[node].product_type))
+            network.nodes[node].node_id,
+            network.nodes[node].product_name,
+            network.nodes[node].product_id,
+            network.nodes[node].product_type))
         print(
-            "{} - Version : {}".format(network.nodes[node].node_id, network.nodes[node].version))
+            "{} - Version : {}".format(network.nodes[node].node_id,
+                                       network.nodes[node].version))
         print("{} - Command classes : {}".format(
-            network.nodes[node].node_id, network.nodes[node].command_classes_as_string))
+            network.nodes[node].node_id,
+            network.nodes[node].command_classes_as_string))
         print("{} - Capabilities : {}".format(
             network.nodes[node].node_id, network.nodes[node].capabilities))
         print(
-            "{} - Neigbors : {}".format(network.nodes[node].node_id, network.nodes[node].neighbors))
+            "{} - Neigbors : {}".format(network.nodes[node].node_id,
+                                        network.nodes[node].neighbors))
         print(
-            "{} - Can sleep : {}".format(network.nodes[node].node_id, network.nodes[node].can_wake_up()))
+            "{} - Can sleep : {}".format(network.nodes[node].node_id,
+                                         network.nodes[node].can_wake_up()))
 
 
-def get_multisensors_node_ids(network):
+def get_multisensors_node_ids(network_obj):
     """Look for multisensor(s) in the ZWave network
         to retrieve the node id of all "multisensor 6" that is found
         If no multisensor is found, the returned value is -1
 
-    :param network: initialized network object
-    :type network: ZWaveNetwork Object
-    :return: Array of Sensors IDs
+    :param network_obj: initialized network object
+    :type network_obj: ZWaveNetwork Object
+    :return: Array of Sensors IDs (node id) or
+             -1 if no Multisensors are found
     :rtype: [int]
     """
     sensor_id = []
 
-    for node in network.nodes:
-        if "MultiSensor 6" in network.nodes[node].product_name:
-            sensor_id.append(network.nodes[node].node_id)
+    for node in network_obj.nodes:
+        if "MultiSensor 6" in network_obj.nodes[node].product_name:
+            sensor_id.append(network_obj.nodes[node].node_id)
     if len(sensor_id) > 0:
         return sensor_id
     else:
         return -1
 
 
-def is_multisensor_awake(sensor_id, network):
+def is_multisensor_awake(sensor_id, network_obj):
     """Checks if a multisensor 6 is awake (True) or
     sleeping (False)
 
     :param sensor_id: network.nodes.node_id
-    :type sensor_id: netowrk object
-    :param network: node id
-    :type network: OpenZWave Network object
-    :return: Bool, Node_id
-    :rtype: Bool, Int
+    :type sensor_id: int
+    :param network_obj: network object
+    :type network_obj: OpenZWave Network object
+    :return: True or False
+    :rtype: Bool
     """
-    multisensor = ZWaveNode(sensor_id, network)
+    multisensor = ZWaveNode(sensor_id, network_obj)
     if multisensor.is_awake:
         return True, sensor_id
     else:
@@ -123,7 +141,12 @@ def is_multisensor_awake(sensor_id, network):
 
 
 if __name__ == "__main__":
-
+    ''' Running this script as main will perform the following:
+        1. Set logging options
+        2. Initialize a Network object with options
+        3. Check if Z-Stick ZWave network is awake
+        4. Collect node ids for all Multisensors in network
+        5. Check if multisensors are Awake or Sleeping '''
     # Set some options for ZWave, logging to file etc.
     options = ZWaveOption(Z_STICK)
     options.set_log_file("OpenZWave_Log.log")
