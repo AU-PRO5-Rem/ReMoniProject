77da0f01dd5fa456d83a8681284ae9ffbf904534
Malthe Brauer-Nielsen <Malthe@bivs.dk>
2020-11-11 14:52:40 +0100

    Implemented convert to and from json functions

--- remoniproject/texthandling/jsonhandler.py
+++ remoniproject/texthandling/jsonhandler.py
@@ -8,8 +8,12 @@ class jsonhandler(Ijsonhandler):
     def __init__(self):
         self.__jsonhandler = Ijsonhandler
 
-    def convertfromjson(self):
-        return NotImplementedError
+    def convertfromjson(self, json_data):
+        string_data = json.loads(json_data)
 
-    def converttojson(self):
-        return NotImplementedError
+        return string_data
+
+    def converttojson(self, string_data):
+        json_data = json.dumps(string_data)
+
+        return json_data
