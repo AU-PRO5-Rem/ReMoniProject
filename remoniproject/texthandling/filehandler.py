45eba9eb694a0834c033938acf28ac4645cd24df
Malthe Brauer-Nielsen <Malthe@bivs.dk>
2020-11-11 14:37:27 +0100

    Implemented read txt file and write to json file

--- remoniproject/texthandling/filehandler.py
+++ remoniproject/texthandling/filehandler.py
@@ -1,12 +1,24 @@
 from remoniproject.texthandling.interfaces import Ifilehandler
-
+from
 
 class filehandler(Ifilehandler):
     def __init__(self, Ifilehandler):
         self.__filehandler = Ifilehandler
 
-    def writetojsonfile(self):
-        return NotImplementedError
+    def writetojsonfile(self, data, ID):
+        filepath = 'remoniproject/data/'    # File path
+        filename = 'Send_sensor'+str(ID)+'.json'   # File name with ID
+        writepath = filepath + filename     # Complete path to file
+        f = open(writepath, 'w+')       # Open file if file doesn't exist create the file
+        f.write(data)       # Write json data in the file
+        f.close()       # Close the file
 
-    def readfromfile(self):
-        return NotImplementedError
+    def readfromfile(self, ID):
+        filepath = 'remoniproject/data/'  # File path
+        filename = 'conf_pnZW100_MultiSensor_6_ni'+str(ID)+'.txt'   # File name with ID
+        writepath = filepath + filename     # Complete path to file
+        f = open(writepath, 'r')        # open file in read mode
+        data = f.read()             # Read content of file
+        f.close()           # Close file
+
+        return data     # return data
