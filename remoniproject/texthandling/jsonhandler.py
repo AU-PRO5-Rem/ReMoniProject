b26fc911a0b56147bca4566d01988c50e75caaf0
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-11-22 18:10:24 +0100

    Made changes to jsonhandler.py and removed Itexthandler.py

--- remoniproject/texthandling/jsonhandler.py
+++ remoniproject/texthandling/jsonhandler.py
@@ -1,19 +1,40 @@
-from remoniproject.texthandling.interfaces import Ijsonhandler
+"""
+    Description:    Converts to and from json format and return data
+    Dependencies:   json lib
 
+    Functions:      :convertfromjson - Converts passed string to dict
+                    :converttojson - Converts passed dict to string
 
+"""
+
+from remoniproject.texthandling.interfaces.Ijsonhandler import Ijsonhandler
 import json
 
 
 class jsonhandler(Ijsonhandler):
-    def __init__(self):
-        self.__jsonhandler = Ijsonhandler
 
     def convertfromjson(self, json_data):
+        """
+        Converts to dict from string
+        :param json_data: data in string format
+        :return: data in dict format
+        """
+
+        # Converts data with json.loads function
         string_data = json.loads(json_data)
 
+        # Returns converted data
         return string_data
 
     def converttojson(self, string_data):
+        """
+        Converts to string from dict
+        :param string_data: data in dict format
+        :return: data in string format
+        """
+
+        # Converts data with json.dumps funtion
         json_data = json.dumps(string_data)
 
+        # Returns converted data
         return json_data
