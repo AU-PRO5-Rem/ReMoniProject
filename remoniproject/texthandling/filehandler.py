eea629da16f480bf336bd3c69dd5dc5caa2061ff
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-11-22 18:14:12 +0100

    Made changes to filehandler.py

--- remoniproject/texthandling/filehandler.py
+++ remoniproject/texthandling/filehandler.py
@@ -1,35 +1,65 @@
-from remoniproject.texthandling.interfaces import Ifilehandler
-import fcntl
+"""
+    Description:    writes to and reads from file
+    Dependencies:   fcntl
+
+    Functions:      :readfile - reads data from file
+                    :writefile - Writes data to file
+
+"""
+
+from remoniproject.texthandling.interfaces.Ifilehandler import Ifilehandler
+import fcntl    # package with file lock function
 
 
 class filehandler(Ifilehandler):
-    def __init__(self, Ifilehandler):
-        self.__filehandler = Ifilehandler
 
     def readfile(self, path):
+        """
+        Reads from file
+        :param path: path to where file is located
+        :return: returns file data
+        """
 
+        # Opens file
         with open(path, "r") as filepointer:
+            # Local lock variable
             lock = 0
+            # while loop to wait for lock to be available
             while lock != 1:
+                # Tries to lock file so other process can't use it
                 try:
+                    # Lock function, throws exception if file is already locked
                     fcntl.flock(filepointer, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     lock = 1
+                # Catch thrown error
                 except IOError:
                     lock = 0
-            data = filepointer
+            # Reads data from file
+            data = filepointer.read()
+            # Releases lock
             fcntl.flock(filepointer, fcntl.LOCK_UN)
-
+        #returns data
         return data
 
     def writefile(self, path, data):
+        """
+        Write to file
+        :param path: path including filename
+        :param data: data going to be written to file
+        :return: None
+        """
 
+        # Opens file
         with open(path, "w") as filepointer:
             lock = 0
+            # Tries to get lock for the file
             while lock != 1:
                 try:
                     fcntl.flock(filepointer, fcntl.LOCK_EX | fcntl.LOCK_NB)
                     lock = 1
                 except IOError:
                     lock = 0
+            # Writes to file
             filepointer.write(data)
+            # Releases lock
             fcntl.flock(filepointer, fcntl.LOCK_UN)
