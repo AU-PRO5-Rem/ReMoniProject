cbb58008db6b4ce4f44bc500530572f9fd855045
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-11-23 10:55:37 +0100

    Implemented fake_filehandler.py

--- remoniproject/texthandling/fakes/fake_filehandler.py
+++ remoniproject/texthandling/fakes/fake_filehandler.py
@@ -1,13 +1,17 @@
-from remoniproject.texthandling.interfaces import Ifilehandler
+from texthandling.interfaces.Ifilehandler import Ifilehandler
 
 
 class fake_filehandler(Ifilehandler):
 
-    def __init__(self):
-        pass
+    def readfile(self, path):
+        data = {
+            "Temperature": 71.0,
+            "Luminance": 55.0,
+            "Relative Humidity": 51.0,
+            "Ultraviolet": 0.0,
+            "Timestamp": "2020-11-18 11:37:39.727683"
+        }
+        return data
 
-    def writetojsonfile(self):
-        return NotImplementedError
-
-    def readfromfile(self):
-        return NotImplementedError
+    def writefile(self, file, data):
+        return
