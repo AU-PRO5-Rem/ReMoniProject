55036e6740d7aaef3c7ad416836c2068021967ed
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-11-24 19:02:41 +0100

    Fixed ninja lint error

--- remoniproject/texthandling/fakes/fake_filehandler.py
+++ remoniproject/texthandling/fakes/fake_filehandler.py
@@ -4,14 +4,14 @@ from remoniproject.texthandling.interfaces.Ifilehandler import Ifilehandler
 class fake_filehandler(Ifilehandler):
 
     def readfile(self, path):
-        data = {    # pragma: no cover
+        data = {  # pragma: no cover
             "Temperature": 71.0,
             "Luminance": 55.0,
             "Relative Humidity": 51.0,
             "Ultraviolet": 0.0,
             "Timestamp": "2020-11-18 11:37:39.727683"
         }
-        return data # pragma: no cover
+        return data  # pragma: no cover
 
     def writefile(self, file, data):
         return
